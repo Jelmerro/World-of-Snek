897349ab18e38a925975ae215369a2cae57cda6a
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2019-07-13 13:40:37 +0200

    add colored server output + automatically serve client www

---
+++ server/www.js
@@ -0,0 +1,58 @@
+/*
+* World of Snek - Snake Battle Royale
+* Copyright (C) 2019 Jelmer van Arnhem
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+"use strict"
+
+const http = require("http")
+const fs = require("fs")
+require("colors")
+
+const serve = () => {
+    http.createServer((req, res) => {
+        const requestUrl = `../client/${req.url}`
+        const requestUrlIndex = `../client/${req.url}/index.html`
+        console.log(`${res.socket.remoteAddress} - ${req.url}`.grey)
+        try {
+            if (isFile(requestUrl)) {
+                fs.createReadStream(requestUrl).pipe(res)
+                res.writeHead(200)
+            } else if (isFile(requestUrlIndex)) {
+                fs.createReadStream(requestUrlIndex).pipe(res)
+                res.writeHead(200)
+            } else {
+                res.writeHead(404)
+                res.end()
+            }
+        } catch (e) {
+            console.log(e.toString().red)
+        }
+    }).listen(8000)
+}
+
+const isFile = f => {
+    try {
+        return fs.existsSync(f) && fs.lstatSync(f).isFile()
+    } catch (e) {
+        return false
+    }
+}
+
+if (module.parent) {
+    module.exports = serve
+} else {
+    serve()
+}
