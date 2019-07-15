1c19d3f462811fe206a6e02d3d900fe257900bce
May <M4Yt@users.noreply.github.com>
2019-07-15 22:25:20 +0200

    made it possible to add players and change controls

--- client/ui.js
+++ client/ui.js
@@ -19,7 +19,8 @@
 "use strict"
 
 // hmm lekker uitje
-/* global M connectSocket gameData serverIP serverPort localPlayers */
+/* global M connectSocket gameData serverIP serverPort localPlayers controls
+makeNewPlayer */
 
 const playerlist = document.getElementById("player-list")
 const areaShrink = document.getElementById("area-shrink")
@@ -46,18 +47,24 @@ const maxEntitiesControl = document.getElementById("max-entities")
 const controlSelect = document.getElementById("control-select")
 const playerColorField = document.getElementById("player-color")
 const playerNameField = document.getElementById("player-name")
+const controlUp = document.getElementById("up")
+const controlDown = document.getElementById("down")
+const controlLeft = document.getElementById("left")
+const controlRight = document.getElementById("right")
 
 let lastPlayers
 let lastCountdown
 
+//for materialize
+const modals = document.querySelectorAll(".modal")
+const selects = document.querySelectorAll("select")
+
 // init modals
 document.addEventListener("DOMContentLoaded", () => {
-    const modals = document.querySelectorAll(".modal")
     const modalInstances = M.Modal.init(modals, {dismissible: false})
     menuModal = M.Modal.getInstance(menu)
     connectModal = M.Modal.getInstance(connect)
     addPlayerModal = M.Modal.getInstance(addPlayer)
-    const selects = document.querySelectorAll("select")
     const selectInstances = M.FormSelect.init(selects)
 })
 
@@ -141,7 +148,12 @@ maxEntitiesControl.addEventListener("change", () => {
 })
 
 controlSelect.addEventListener("change", () => {
-    console.log(controlSelect.value)
+    if (controls[controlSelect.value]) {
+        fillControlFields(controls[controlSelect.value])
+    } else {
+        emptyControlFields()
+    }
+    M.updateTextFields()
 })
 
 function connectFromModal() {
@@ -223,6 +235,7 @@ function updateServerSettings() {
         serverInfo.textContent = `Connected to ${serverIP}:${serverPort}`
     }
     serverSettings.appendChild(serverInfo)
+    serverSettings.appendChild(document.createElement("br"))
     const button = document.createElement("button")
     button.textContent = "Change"
     button.classList.add("btn", "waves-effect", "waves-light")
@@ -269,25 +282,76 @@ function updateAddPlayer(edit = false, player) {
     if (edit) {
         title.textContent = "Edit a player"
         playerColorField.value = player.color
+        playerColorField.disabled = true
         playerNameField.value = player.name
+        playerNameField.disabled = true
         if (player.controls.preset) {
             controlSelect.value = player.controls.preset
         } else {
             controlSelect.value = "Custom"
         }
+        fillControlFields(player.controls)
         // onclick to override function
         addPlayerButton.onclick = () => {
-            console.log("you just edited a player")
+            const editedPlayer = localPlayers.find(p => p.uuid === player.uuid)
+            if (controls[controlSelect.value]) {
+                Object.assign(editedPlayer.controls, controls[controlSelect.value])
+            } else {
+                Object.assign(editedPlayer.controls, getControls())
+            }
+            addPlayerModal.close()
         }
     } else {
         title.textContent = "Add a new player"
         playerColorField.value = null
+        playerColorField.disabled = false
         playerNameField.value = null
+        playerNameField.disabled = false
         controlSelect.value = "Choose a set of controls"
+        emptyControlFields()
         // onclick to override function
         addPlayerButton.onclick = () => {
-            console.log("you just added a player")
+            const [name, color] = getNameAndColor()
+            let ctrls
+            if (controls[controlSelect.value]) {
+                ctrls = controls[controlSelect.value]
+            } else {
+                ctrls = getControls()
+            }
+            const newPlayer = makeNewPlayer(name, color, ctrls)
+            localPlayers.push(newPlayer)
+            addPlayerModal.close()
+            updatePlayerSettings()
         }
     }
     M.updateTextFields()
+    M.FormSelect.init(selects)
+}
+
+function fillControlFields(controls) {
+    controlUp.value = controls.up
+    controlDown.value = controls.down
+    controlLeft.value = controls.left
+    controlRight.value = controls.right
+}
+
+function emptyControlFields() {
+    controlUp.value = null
+    controlDown.value = null
+    controlLeft.value = null
+    controlRight.value = null
+}
+
+function getNameAndColor() {
+    return [playerNameField.value, playerColorField.value]
+}
+
+function getControls() {
+    return {
+        preset: null,
+        up: controlUp.value,
+        down: controlDown.value,
+        left: controlLeft.value,
+        right: controlRight.value
+    }
 }
